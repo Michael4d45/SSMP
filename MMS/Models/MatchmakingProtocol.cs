b908bc150ad012bcb121055a5926ca214cd2309b
Extremelyd1 <10898310+Extremelyd1@users.noreply.github.com>
2026-07-13 20:46:31 +0200

    Bump version numbers - SSMP and SSMPServer to 0.2.0.0 - MMS to 2

--- MMS/Models/MatchmakingProtocol.cs
+++ MMS/Models/MatchmakingProtocol.cs
@@ -7,7 +7,7 @@ internal static class MatchmakingProtocol {
     /// <summary>
     /// The current matchmaking protocol version required for MMS matchmaking operations.
     /// </summary>
-    public const int CurrentVersion = 1;
+    public const int CurrentVersion = 2;
 
     /// <summary>
     /// Error code returned when a client must update before using matchmaking.
